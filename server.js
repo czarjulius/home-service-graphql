d67498a11cd5c4f43a5a00e1d1298519d1a42e1e
czarjulius <juliusczar.jc@gmail.com>
2020-08-17 00:11:30 +0200

    added mongodb

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const {graphqlHTTP} = require("express-graphql")
-const schema = require('./schema/schema')
+// const {graphqlHTTP} = require("express-graphql")
+// const mongoConnect = require("./util/database");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use('/graphql', graphqlHTTP({
+//   schema,
+//   graphiql: true
+// }));
 
-app.use('/graphql', graphqlHTTP({
-  schema, 
-  graphiql: true
-}));
+// app.listen(4000, () => {
+//   console.log("Listening");
+// });
 
-app.listen(4000, () => {
-  console.log("Listening");
-});
+// mongoConnect(() => {
+//   app.listen(4000);
+// });
+
+mongoose
+  .connect(
+    "mongodb+srv://julius:julius@cluster0.4pjb6.mongodb.net/home_service?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(4000);
+  })
+  .catch((err) => console.log(err));
---
+++ util/database.js
@@ -0,0 +1,28 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://julius:julius@cluster0.4pjb6.mongodb.net/home_service?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected Successfully!");
+      _db = client.db();
+      callback();
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
